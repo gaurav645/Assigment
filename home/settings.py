0d5975f720534450f87503cb8fbf14ff2d42f80d
Gaurav Kumar <66291225+gaurav645@users.noreply.github.com>
2023-02-12 16:35:56 +0530

    Setting.py modificatins

--- home/settings.py
+++ home/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n-^!w!c=7egmzu11mj7ugs^!2bg&h6qc^m0ef9**zg=cc&#bxn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.Emsecassesment.up.railway.app']
 
 
 # Application definition
