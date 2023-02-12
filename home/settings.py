4dcb95b00f02ea21309963aae635387d2c461b82
Gaurav Kumar <66291225+gaurav645@users.noreply.github.com>
2023-02-12 16:21:28 +0530

    Hosting

--- home/settings.py
+++ home/settings.py
@@ -131,3 +131,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+# Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
